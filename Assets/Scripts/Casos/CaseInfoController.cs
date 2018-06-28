64a42c6c29380b3395ed3c9babce2cded57f1ae9
Jaime Hernández <jaimevazz@gmail.com>
2018-06-28 18:12:13 -0500

    Case Info begin

---
+++ Assets/Scripts/Casos/CaseInfoController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaseInfoController : MonoBehaviour 
+{
+	[SerializeField] private CaseInfoData caseInfoData;
+	
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
