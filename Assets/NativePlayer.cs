f4db65e534a9b475007425116deda36e03ed9ad6
Jaime Hernandez <jaimevazz@gmail.com>
2019-01-24 12:58:57 -0600

    Play Video in Native Player

---
+++ Assets/NativePlayer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NativePlayer : MonoBehaviour {
+
+	public void PlayVideoFullscreen(string name)
+    {
+        Handheld.PlayFullScreenMovie(name, Color.black, FullScreenMovieControlMode.Full, FullScreenMovieScalingMode.AspectFit);
+    }
+}
