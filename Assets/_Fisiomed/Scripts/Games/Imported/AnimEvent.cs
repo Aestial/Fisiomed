3706c007e73ba0f13a3b7a117d3aa5c632f89fe2
Jaime Hernández <jaimevazz@gmail.com>
2019-01-29 00:59:06 -0600

    Quiz (and FIrst Game Started)

---
+++ Assets/_Fisiomed/Scripts/Games/Imported/AnimEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimEvent : MonoBehaviour 
+{
+	public GameState gameState;
+
+	public void SetState(int index)
+	{
+		gameState.CurrentState = index;
+	}
+}
---
+++ Assets/_Fisiomed/Scripts/Games/Imported/GameState.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameState : MonoBehaviour 
+{
+	[SerializeField] GameObject[] stateContainers;
+	[SerializeField] int currentState = 0;
+
+	public int CurrentState 
+	{
+		get { return currentState; }
+		set { SetState(value); }
+	}
+	
+	public void ResetLevel()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+	public void SetState(int index)
+	{
+		Debug.Log("Turning into State: " + currentState);
+		ActiveAll(false);
+		ActiveContainer(index, true);
+		currentState = index;
+	}
+
+	private void ActiveContainer(int index, bool active)
+	{
+		stateContainers[index].SetActive(active);
+	}
+
+	private void ActiveAll(bool active)
+	{
+		for (int i = 0; i < stateContainers.Length; i++)
+		{
+			ActiveContainer(i, active);
+		}
+	}
+}
