ca5b84ac9ffb5b63c2ecd45b1ccd3a3f8ac4068e
Jaime Hernández <jaimevazz@gmail.com>
2019-06-19 07:57:37 -0500

    Bubble Controller and Downloader helper

---
+++ Assets/_Fisiomed/Scripts/Chat/BubbleController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Fisiomed.Chat
+{
+    public enum BubbleSide
+    {
+        Left,
+        Right
+    }
+
+    public class BubbleController : MonoBehaviour
+    {
+        
+        [Header("Current Values")]
+        public string content;
+        public Sprite characterSprite;
+        public BubbleSide side;
+        public float radius;
+        public Color charBColor;
+        public Color textBColor;
+        public Color textColor;
+        [Header("Customize Components")]
+        [SerializeField] Image characterImage;
+        [SerializeField] Image characterBubble;
+        [SerializeField] Image textBubble;
+        [SerializeField] Image textBubbleShadow;
+        [SerializeField] Text text;
+
+        public void SetAll()
+        {
+            SetCharacter(characterSprite);
+            SetColors(charBColor, textBColor, textColor);
+            SetSide(side, radius);
+            SetContent(content);
+
+        }
+
+        void SetContent(string content)
+        {
+            string description = "Dianita me nombró <b>" + characterImage.sprite.name 
+                                    + "</b> y tengo el color: <b>" + charBColor.ToString() 
+                                    + "</b>";
+            text.text = content;
+        }
+
+
+        void SetCharacter(Sprite sprite)
+        {
+            characterImage.sprite = sprite;
+        }
+
+        void SetColors(Color charBColor, Color textBColor, Color textColor)
+        {
+            characterBubble.color = charBColor;
+            textBubble.color = textBColor;
+            text.color = textColor;
+        }
+
+        void SetSide(BubbleSide side, float radius)
+        {
+            Vector4 radiusRect = new Vector4(radius, radius, radius, radius);
+            switch(side)
+            {
+                case BubbleSide.Left:
+                    radiusRect.x = 0;
+                    break;
+                case BubbleSide.Right:
+                    transform.GetChild(0).SetAsLastSibling();
+                    radiusRect.y = 0;
+                    break;
+            }
+            textBubble.GetComponent<FreeModifier>().Radius = radiusRect;
+            textBubbleShadow.GetComponent<FreeModifier>().Radius = radiusRect;
+        }
+    }    
+}
---
+++ Assets/_Fisiomed/Scripts/Helpers/Downloader.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Fisiomed.Loader
+{
+    public class Downloader : Singleton<Downloader>
+    {
+        public IEnumerator LoadJSON(string uri, Action<string> onCompleted)
+        {
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+            {
+                // Request and wait for the desired page.
+                yield return webRequest.SendWebRequest();
+
+                if (webRequest.isNetworkError || webRequest.isHttpError)
+                {
+                    Debug.Log(uri + ": Error: " + webRequest.error);
+                }
+                else
+                {
+                    string text = webRequest.downloadHandler.text;
+                    Debug.Log(uri + ":\nReceived: " + text);
+                    // Callback function
+                    onCompleted(text);
+                }
+            }
+        }
+
+        public IEnumerator LoadAsset(string uri, Action<GameObject> onCompleted)
+        {
+            Debug.Log("Downloader - Loading Asset");
+            using (UnityWebRequest webRequest = UnityWebRequestAssetBundle.GetAssetBundle(uri))
+            {
+                // Request and wait for the desired page.
+                yield return webRequest.SendWebRequest();
+                // Fail
+                if (webRequest.isNetworkError || webRequest.isHttpError)
+                {
+                    Debug.Log("Downloader - Error: " + webRequest.error + " from: " + uri);
+                }
+                // Success
+                else
+                {
+                    // Load and retreive the AssetBundle
+                    AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(webRequest);
+                    Debug.Log("Downloader - Received: " + bundle + " from: " + uri);
+
+                    // Load the object asynchronously
+                    AssetBundleRequest request = bundle.LoadAssetAsync(bundle.GetAllAssetNames()[0], typeof(GameObject));
+                    yield return request;
+
+                    // Load the object synchronously
+                    //GameObject prefab = bundle.LoadAsset(bundle.GetAllAssetNames()[0]) as GameObject;
+                    //yield return prefab;
+
+                    GameObject prefab = request.asset as GameObject;
+
+                    Debug.Log("Downloader - Loaded: " + prefab);
+
+                    bundle.Unload(false);
+                    // Callback function
+                    onCompleted(prefab);
+                   
+                }
+                webRequest.Dispose();
+            }
+        }
+
+        public IEnumerator LoadVideo(string uri, string filename, Action<string> onCompleted)
+        {
+            Debug.Log("Downloader - Loading Video");
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+            {
+                // Request and wait for the desired page.
+                yield return webRequest.SendWebRequest();
+
+                if (webRequest.isNetworkError || webRequest.isHttpError)
+                {
+                    Debug.Log("Downloader - Error: " + webRequest.error + " from: " + uri);
+                }
+                else
+                {
+                    string filePath = Path.Combine(Application.persistentDataPath, filename);
+
+                    File.WriteAllBytes(filePath, webRequest.downloadHandler.data);
+                    Debug.Log("VideoDonload - Video saved: " + filePath);
+
+                    // Callback function
+                    onCompleted(filePath);
+
+                }
+                webRequest.Dispose();
+            }
+        }
+    }
+}
