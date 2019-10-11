aedb94c7806471a3a74bf5e92213b7732d604b7f
Jaime Hernández <jaimevazz@gmail.com>
2019-10-11 11:27:46 -0500

    Update PresentationController.cs

--- Assets/_Fisiomed/Modules/Presentation/PresentationController.cs
+++ Assets/_Fisiomed/Modules/Presentation/PresentationController.cs
@@ -1,27 +1,29 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using Fisiomed.Case;
 using Loader;
 
-public class PresentationController : Singleton<PresentationController>
+namespace Fisiomed.Case
 {
-    [SerializeField] Canvas canvas;
-    [SerializeField] TMP_Text text;
-    [SerializeField] Image image;
-    public void Set(Info info)
-    {                
-        StartCoroutine(Downloader.Instance.LoadTexture(info.imageUrl, OnTextureLoaded));
-        text.text = TextProcess.Process(info.description);
-    }
-    public void Close()
+    public class PresentationController : Singleton<PresentationController>
     {
-        canvas.enabled = false;
-    }
-    void OnTextureLoaded(Texture texture)
-    {
-        Texture2D tex = (Texture2D) texture;
-        Sprite sprite = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f), 100.0f);		
-        image.sprite = sprite;        
+        [SerializeField] Canvas canvas;
+        [SerializeField] TMP_Text text;
+        [SerializeField] Image image;
+        public void Set(Info info)
+        {                
+            StartCoroutine(Downloader.Instance.LoadTexture(info.imageUrl, OnTextureLoaded));
+            text.text = TextProcess.Process(info.description);
+        }
+        public void Close()
+        {
+            canvas.enabled = false;
+        }
+        void OnTextureLoaded(Texture texture)
+        {
+            Texture2D tex = (Texture2D) texture;
+            Sprite sprite = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f), 100.0f);		
+            image.sprite = sprite;        
+        }
     }
 }
