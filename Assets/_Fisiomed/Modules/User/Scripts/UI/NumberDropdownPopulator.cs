72488ce44412e2af678737285648b302024f70fd
Jaime Hernandez <jaimevazz@gmail.com>
2020-01-25 12:04:34 -0600

    +UI Elements Helper Functions

---
+++ Assets/_Fisiomed/Modules/User/Scripts/UI/NumberDropdownPopulator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberDropdownPopulator : MonoBehaviour
+{
+    [SerializeField] TMP_Dropdown dropdown;
+    [SerializeField] int start;
+    [SerializeField] int end;
+    List<string> options;
+    void Start()
+    {
+        options = new List<string>();
+        for (int i = start; i <= end; i++)
+        {
+            string option = i.ToString();
+            options.Add(option);
+        }
+        dropdown.AddOptions(options);
+    }
+}
---
+++ Assets/_Fisiomed/Modules/User/Scripts/UI/PasswordUncover.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class UncoverPassword : MonoBehaviour
+{
+    [SerializeField] TMP_InputField inputField;
+    bool isCovered = true;
+    public void ToggleCover(bool isActive)
+    {
+        if (isCovered) Uncover();
+        else Cover();
+
+        inputField.ActivateInputField();
+        inputField.DeactivateInputField();
+    }
+    private void Cover()
+    {
+        isCovered = true;
+        inputField.contentType = TMP_InputField.ContentType.Password;
+    }
+    private void Uncover()
+    {
+        isCovered = false;
+        inputField.contentType = TMP_InputField.ContentType.Standard;
+    }    
+}
