4b0e3e098507269ef5595e5c63d6821799601a80
Jaime Hernandez <jaimevazz@gmail.com>
2020-01-25 11:58:24 -0600

    Added: User Model

---
+++ Assets/_Fisiomed/Modules/User/Scripts/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Fisiomed.User
+{
+    [Serializable]
+    public class User
+    {
+        public string id; // DB ID
+        public string email; // Unique identifier
+        public string password;
+        public GameProfile gamer;
+        public PersonalProfile personal;
+    }
+    [Serializable]
+    public class GameProfile
+    {
+        // Game information
+        public string username;
+        public string avatar;
+        // TODO: PROGRESS
+        public int level;
+    }    
+    [Serializable]
+    public class PersonalProfile
+    {
+        // Real name
+        public string name;
+        public string surname;
+        // User personal info
+        public string gender;
+        public string age;
+        public string carreer;
+        public string university;
+        public string semester;
+        public string unamID;
+    }
+}
