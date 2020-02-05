90b3f68dbcab3f9b4d23d214ce80f1c0e3edb243
Jaime Hernandez <jaimevazz@gmail.com>
2020-02-05 01:41:11 -0600

    Updated AppManager for Loader Control

--- Assets/_Fisiomed/Modules/Core/AppManager.cs
+++ Assets/_Fisiomed/Modules/Core/AppManager.cs
@@ -1,104 +1,96 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 
 public class AppManager : Singleton<AppManager> 
 {
 	public bool iAmFirst;
 	[SerializeField] private GameObject loaderPrefab;
-	private Canvas loaderCanvas;
+    LoaderController loader;
 
-	private int m_CurrentScene;
-	private string m_CurrentSceneName;
-
-	// void Awake()
-    // {
-	// 	#region Don't Destroy OnLoad Singleton
-	// 	DontDestroyOnLoad(Instance);
-	// 	AppManager[] appManagers = FindObjectsOfType(typeof(AppManager)) as AppManager[];
-	// 	if(appManagers.Length > 1)
-	// 	{
-	// 		for(int i = 0; i < appManagers.Length; i++)
-	// 		{
-	// 			if(!appManagers[i].iAmFirst)
-	// 				DestroyImmediate(appManagers[i].gameObject);
-	// 		}
-	// 	}
-	// 	else
-	// 		iAmFirst = true;
-	// 	#endregion
-    // }
-
-    // called first
+    // Called first
+    void Awake()
+    {
+        #region Don't Destroy OnLoad Singleton
+        DontDestroyOnLoad(Instance);
+        AppManager[] appManagers = FindObjectsOfType(typeof(AppManager)) as AppManager[];
+        if (appManagers.Length > 1)
+        {
+            for (int i = 0; i < appManagers.Length; i++)
+            {
+                if (!appManagers[i].iAmFirst)
+                    DestroyImmediate(appManagers[i].gameObject);
+            }
+        }
+        else
+            iAmFirst = true;
+        #endregion
+    }
+    // Called second
     void OnEnable()
     {
         // Debug.Log("OnEnable called");
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
-    // called second
+    // Called third
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         Debug.Log("OnSceneLoaded: " + scene.name);
-
-		LoaderController loader = FindObjectOfType<LoaderController>();
-		
-		if (loader) {
-			this.loaderCanvas = loader.GetComponent<Canvas>();
-		} else {
-			GameObject newLoader = Instantiate(loaderPrefab);
-			this.loaderCanvas = newLoader.GetComponent<Canvas>();
-		}
-		
-		this.loaderCanvas.enabled = false;
+		if (!loader)
+        {
+            GameObject newLoader = Instantiate(loaderPrefab, this.transform);
+            loader = newLoader.GetComponent<LoaderController>();
+		}	
+		loader.SetLoader(false);
     }
-    // called when the game is terminated
+    // Called when the game is terminated
     void OnDisable()
     {
         // Debug.Log("OnDisable");
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
-
 	private IEnumerator LoadAndChangeCoroutine(int scene, float time)
 	{
-		this.loaderCanvas.gameObject.SetActive(false);
-		yield return null;
-		this.loaderCanvas.gameObject.SetActive(true);
-		this.loaderCanvas.enabled = true;
-		yield return new WaitForSeconds(time);
+		loader.SetLoader(true);
+        yield return new WaitForSeconds(time);
 		SceneManager.LoadScene(scene);
 	}
 	private IEnumerator LoadAndChangeCoroutine(string scene, float time)
 	{
-		this.loaderCanvas.gameObject.SetActive(false);
-		yield return null;
-		this.loaderCanvas.gameObject.SetActive(true);
-		this.loaderCanvas.enabled = true;
-		yield return new WaitForSeconds(time);
+        loader.SetLoader(true);
+        yield return new WaitForSeconds(time);
 		SceneManager.LoadScene(scene);
 	}
 	public void ChangeScene (int scene)
 	{
-		this.m_CurrentScene = scene;
 		StartCoroutine(this.LoadAndChangeCoroutine(scene, 1.1f));
 	}
 	public void ChangeScene (string sceneName)
 	{
-		this.m_CurrentSceneName = sceneName;
 		StartCoroutine(this.LoadAndChangeCoroutine(sceneName, 1.1f));
 	}
 	public void ShowLoader(bool show)
 	{
-		this.loaderCanvas.enabled = show;
-	}
+        loader.SetLoader(show);
+    }
     public void ShowLoaderDelay(bool show, float delay)
     {
         StartCoroutine(ShowLoaderCoroutine(show, delay)); 
     }
+    public void ShowLoaderDelayCallback(bool show, float delay, Action onCompleted)
+    {
+        StartCoroutine(ShowLoaderCoroutine(show, delay, onCompleted));
+    }
     public IEnumerator ShowLoaderCoroutine(bool show, float delay)
     {
         yield return new WaitForSeconds(delay);
-        this.loaderCanvas.enabled = show;
+        loader.SetLoader(show);
+    }
+    public IEnumerator ShowLoaderCoroutine(bool show, float delay, Action onCompleted)
+    {
+        yield return new WaitForSeconds(delay);
+        loader.SetLoader(show);
+        onCompleted();
     }
 }
