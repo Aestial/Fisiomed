d3f2f074174c90838304f4a4065cb2a219d296b1
Jaime Hernandez <jaimevazz@gmail.com>
2020-02-24 11:45:49 -0600

    Display in MainMenu

---
+++ Assets/_Fisiomed/Modules/User/Scripts/UserDisplay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Fisiomed.User
+{
+    public class UserDisplay : MonoBehaviour
+    {
+        [SerializeField] new TMP_Text name;
+
+        void Start()
+        {
+            name.text = UserManager.Instance.User.personal.name;
+        }
+    }
+}
