594aa9ad4967d10c1ac8ca1401c32ee73abfc9fc
Jaime Hernandez <jaimevazz@gmail.com>
2020-02-24 18:03:21 -0600

    LOCAL Complete Register

--- Assets/_Fisiomed/Modules/User/Scripts/Validators/Page/PageValidator.cs
+++ Assets/_Fisiomed/Modules/User/Scripts/Validators/Page/PageValidator.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Fisiomed.UI.Validator
@@ -6,23 +7,34 @@ namespace Fisiomed.UI.Validator
     public class PageValidator : MonoBehaviour, IValidator
     {
         [SerializeField] Button validButton;
-        IValidatable[] validatables;
+        List<IValidatable> validatables = new List<IValidatable>();
         bool isValid;
-        void Awake()
+        void Start()
         {
             validButton.interactable = false;
-            validatables = GetComponentsInChildren<IValidatable>();
-            Log.Color("Validatables found in " + gameObject.name + ": " + validatables.Length, this);
-            for (int i = 0; i < validatables.Length; i++)
+            IValidatable[] vs = GetComponentsInChildren<IValidatable>();            
+            Log.Color("Validatables found in " + gameObject.name + ": " + vs.Length, this);
+            validatables = new List<IValidatable>(vs);
+            for (int i = 0; i < validatables.Count; i++)
             {
                 validatables[i].Validator = this;
                 Log.Color("Validatable " + i + ": " + validatables[i], this);
             }
         }
+        public void Suscribe(IValidatable validatable)
+        {
+            validatable.Validator = this;
+            validatables.Add(validatable);            
+        }
+        public void Unsuscribe(IValidatable validatable)
+        {
+            validatable.Validator = null;
+            validatables.Remove(validatable);
+        }
         public void Validate()
         {
             isValid = true;
-            for (int i = 0; i < validatables.Length; i++)
+            for (int i = 0; i < validatables.Count; i++)
             {
                 isValid &= validatables[i].IsValid;
             }
