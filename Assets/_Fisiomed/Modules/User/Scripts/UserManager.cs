9ef6ff4084b9ac9406ef23510fe6ae9f684e380a
Jaime Hernandez <jaimevazz@gmail.com>
2020-06-11 13:03:20 -0500

    Updated Auth scene with saving User info locally

--- Assets/_Fisiomed/Modules/User/Scripts/UserManager.cs
+++ Assets/_Fisiomed/Modules/User/Scripts/UserManager.cs
@@ -1,26 +1,54 @@
 ﻿using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Loader;
 
 namespace Fisiomed.User
 {
     public class UserManager : Singleton<UserManager>
     {
         [SerializeField] string filename;
-        public User User = new User();
+        public User User;
         string filePath;
-        void Start()
-        {
-            string path = Application.persistentDataPath;
+        void Awake()
+        {            
             //string path = Path.Combine(Application.persistentDataPath, "user");
             //if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+            string path = Application.persistentDataPath;
             filePath = Path.Combine(path, filename);
+            if (File.Exists(filePath))
+            {
+                Log.Color("File path:" + filePath, this);
+                StartCoroutine(Downloader.Instance.LoadJSON(filePath, OnDataLoaded));
+            }
+            else
+            {
+                User = new User();
+            }                
+        }
+        //void OnEnable()
+        //{
+        //    SceneManager.sceneLoaded += OnSceneLoaded;
+        //}
+        //void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        //{
+        //    Log.Color("On Scene Loaded: " + scene.name, this);
+        //    StartCoroutine(Downloader.Instance.LoadJSON(filePath, OnDataLoaded));
+        //}
+        //void OnDisable()
+        //{
+        //    SceneManager.sceneLoaded -= OnSceneLoaded;
+        //}
+        private void OnDataLoaded(string json)
+        {
+            User = JsonUtility.FromJson<User>(json);
         }
         void Save(User u)
         {
             string json = JsonUtility.ToJson(u);
             Log.Color(json, this);
             File.WriteAllText(filePath, json);
-        }
+        }        
         public void Save()
         {
             Save(User);
