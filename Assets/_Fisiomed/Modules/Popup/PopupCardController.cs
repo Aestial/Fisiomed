3859a24751150f9ddb86730478277946fb34808b
Jaime Hernandez <jaimevazz@gmail.com>
2020-06-29 15:16:20 -0500

    Added Popup Module

---
+++ Assets/_Fisiomed/Modules/Popup/PopupCardController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class PopupCardController : MonoBehaviour
+{
+    [SerializeField] TMP_Text text;
+    [SerializeField] float lifeTime = 8.0f;
+
+    void Start()
+    {
+        StartCoroutine(DestroyCoroutine(lifeTime));        
+    }
+
+    IEnumerator DestroyCoroutine(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        Destroy(gameObject);
+    }
+
+    public void Print(string msg)
+    {
+        text.text = msg;
+    }
+}
---
+++ Assets/_Fisiomed/Modules/Popup/PopupManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Fisiomed.Popup
+{
+    public class PopupManager : Singleton<PopupManager>
+    {
+        [SerializeField] Canvas canvas;
+        [SerializeField] GameObject cardPrefab;
+        [SerializeField] Transform cardContainer;
+
+        public void PrintMessage(string message)
+        {
+            GameObject newCard = Instantiate(cardPrefab, cardContainer);
+            PopupCardController controller = newCard.GetComponent<PopupCardController>();
+            controller.Print(message);
+        }
+
+        public bool iAmFirst;
+
+        void Awake()
+        {
+            DontDestroyOnLoad(Instance);
+            PopupManager[] managers = FindObjectsOfType(typeof(PopupManager)) as PopupManager[];
+
+            if (managers.Length > 1)
+            {
+                for (int i = 0; i < managers.Length; i++)
+                {
+                    if (!managers[i].iAmFirst)
+                    {
+                        DestroyImmediate(managers[i].gameObject);
+                    }
+                }
+            }
+            else
+            {
+                iAmFirst = true;
+            }
+        }
+    }
+}
