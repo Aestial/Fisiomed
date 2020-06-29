dd32e0fa81afa1a06b9d201af35eb4edacd6600f
Jaime Hernandez <jaimevazz@gmail.com>
2020-06-29 00:07:07 -0500

    Updated User data and manager

--- Assets/_Fisiomed/Modules/User/Scripts/UserManager.cs
+++ Assets/_Fisiomed/Modules/User/Scripts/UserManager.cs
@@ -1,57 +1,196 @@
 ﻿using System.IO;
 using UnityEngine;
+using Firebase.Auth;
+using Firebase.Database;
 using UnityEngine.SceneManagement;
 using Loader;
 
 namespace Fisiomed.User
 {
+    using System.Threading.Tasks;
+    using FirebaseServices;
+
     public class UserManager : Singleton<UserManager>
     {
         [SerializeField] string filename;
-        public User User;
-        string filePath;
+        private string filePath;
+
+        //private User _user = new User();
+        public User User { get; set; } = new User();
+
+        private FirebaseDatabase _database;
+        private FirebaseUser _firebaseUser;
+
+        public StringStringDictionary temp;
+
+        public void SetUserProperty(string value, string key)
+        {
+            if (User.properties.ContainsKey(key))
+                User.properties[key] = value;
+            else
+                User.properties.Add(key, value);       
+        }
+
+        public void SetUserLoginData(string value, string key)
+        {
+            if (temp.ContainsKey(key))
+                temp[key] = value;
+            else
+                temp.Add(key, value);
+        }
+
+        public void SignUpUser()
+        {
+            string email = User.properties["email"];
+            string password = User.properties["password"];
+            FirebaseManager.Instance.Auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+                if (task.IsCanceled)
+                {
+                    Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                    return;
+                }
+                if (task.IsFaulted)
+                {
+                    Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                    return;
+                }
+                // Firebase user has been created.
+                //Firebase.Auth.FirebaseUser user = FirebaseManager.Instance.User;
+                FirebaseManager.Instance.User = task.Result;
+
+                Firebase.Auth.UserProfile profile = new Firebase.Auth.UserProfile();                
+                profile.DisplayName = User.properties["name"]; // TODO: Change for Username
+                //profile.PhotoUrl = ; // TODO: Set Photo URL
+
+                FirebaseManager.Instance.User.UpdateUserProfileAsync(profile).ContinueWith(profileTask =>
+                {
+                    if (profileTask.IsCanceled)
+                    {
+                        Debug.LogError("UpdateUserProfileAsync was canceled.");
+                        return;
+                    }
+                    if (profileTask.IsFaulted)
+                    {
+                        Debug.LogError("UpdateUserProfileAsync encountered an error: " + profileTask.Exception);
+                        return;
+                    }
+                    Save(User);
+
+                    Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                        FirebaseManager.Instance.User.DisplayName,
+                        FirebaseManager.Instance.User.UserId);                    
+
+                });
+            });
+        }
+
+        public void LogInUser()
+        {
+            string email = temp["email"];
+            string password = temp["password"];
+            FirebaseManager.Instance.Auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(signTask => {
+                if (signTask.IsCanceled)
+                {
+                    Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                    return;
+                }
+                if (signTask.IsFaulted)
+                {
+                    Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + signTask.Exception);
+                    return;
+                }
+                FirebaseManager.Instance.User = signTask.Result;
+
+                _firebaseUser = FirebaseManager.Instance.User;
+                Debug.LogFormat(
+                    "User signed in successfully: {0} ({1})",
+                    _firebaseUser.DisplayName,
+                    _firebaseUser   .UserId
+                );
+                _database.GetReference(_firebaseUser.UserId).GetValueAsync().ContinueWith(dataTask =>{
+                    if (dataTask.IsFaulted)
+                    {
+                        // Handle the error...
+                    }
+                    else if (dataTask.IsCompleted)
+                    {
+                        DataSnapshot dss = dataTask.Result;
+                        // Do something with snapshot...
+                        OnDataLoaded(dss.GetRawJsonValue());
+                        Save(User);
+                        AppManager.Instance.ChangeScene("MainMenuScene");
+                    }
+                });
+            });
+        }
+
         void Awake()
-        {            
-            //string path = Path.Combine(Application.persistentDataPath, "user");
-            //if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+        {          
+            
+        }
+        private void Start()
+        {
+            _database = FirebaseDatabase.DefaultInstance;
+        }
+        private void OnDataLoaded(string json)
+        {
+            Log.Color(json, this);
+            User = JsonUtility.FromJson<User>(json);
+            Log.Color(User.ToString(), this);
+        }
+        private void Save(User u)
+        {
+            string json = JsonUtility.ToJson(u);
+            SaveJSONLocal(json);
+            SaveJSONDatabase(json);
+        }
+        private void SaveJSONLocal(string json)
+        {
+            Log.Color("Saving JSON Local: " + json, this);
+            File.WriteAllText(filePath, json);
+        }        
+        private void SaveJSONDatabase(string json)
+        {
+            Log.Color("Saving JSON Database: " + json, this);
+            _database.GetReference(_firebaseUser.UserId).SetRawJsonValueAsync(json)
+            .ContinueWith(dbtask =>
+            {
+                AppManager.Instance.ChangeScene("MainMenuScene");
+            });
+        }
+        //public async Task<bool> SaveJSONOnDatabase(string userData)
+        //{
+        //    FirebaseUser fUser = FirebaseManager.Instance.User;
+        //    var dataSnapshot = await _database.GetReference(fUser.UserId).SetRawJsonValueAsync(userData);
+        //    return dataSnapshot.Exists;
+        //}
+
+        void OnEnable()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+        void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            Log.Color("On Scene Loaded: " + scene.name, this);
+
+            //StartCoroutine(Downloader.Instance.LoadJSON(filePath, OnDataLoaded));
+
             string path = Application.persistentDataPath;
             filePath = Path.Combine(path, filename);
+
             if (File.Exists(filePath))
             {
                 Log.Color("File path:" + filePath, this);
-                StartCoroutine(Downloader.Instance.LoadJSON(filePath, OnDataLoaded));
+                OnDataLoaded(File.ReadAllText(filePath));
             }
             else
             {
                 User = new User();
-            }                
+            }
         }
-        //void OnEnable()
-        //{
-        //    SceneManager.sceneLoaded += OnSceneLoaded;
-        //}
-        //void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-        //{
-        //    Log.Color("On Scene Loaded: " + scene.name, this);
-        //    StartCoroutine(Downloader.Instance.LoadJSON(filePath, OnDataLoaded));
-        //}
-        //void OnDisable()
-        //{
-        //    SceneManager.sceneLoaded -= OnSceneLoaded;
-        //}
-        private void OnDataLoaded(string json)
+        void OnDisable()
         {
-            User = JsonUtility.FromJson<User>(json);
-        }
-        void Save(User u)
-        {
-            string json = JsonUtility.ToJson(u);
-            Log.Color(json, this);
-            File.WriteAllText(filePath, json);
-        }        
-        public void Save()
-        {
-            Save(User);
+            SceneManager.sceneLoaded -= OnSceneLoaded;
         }
     }
 }
