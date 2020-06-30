748f0e6a29ab83014e720b2b6e8d73827d68e2f7
Jaime Hernandez <jaimevazz@gmail.com>
2020-06-29 22:59:08 -0500

    Fixed UserManager

--- Assets/_Fisiomed/Modules/User/Scripts/UserManager.cs
+++ Assets/_Fisiomed/Modules/User/Scripts/UserManager.cs
@@ -10,13 +10,14 @@ namespace Fisiomed.User
     using App;
     using FirebaseServices;
     using Popup;
+    using System;
 
     public class UserManager : Singleton<UserManager>
     {
         [SerializeField] string filename;
 
         private string filePath;
-        private readonly StringStringDictionary temp;
+        private readonly StringStringDictionary temp = new StringStringDictionary();
 
         private FirebaseDatabase _database;
         private FirebaseUser _firebaseUser;
@@ -51,98 +52,83 @@ namespace Fisiomed.User
                 temp.Add(key, value);
         }
         
-        public void SignUpUser()
+        public async void SignUp()
         {
             string email = User.properties["email"];
             string password = User.properties["password"];
             Log.Color("Signing Up User: " + email, this);
-            FirebaseManager.Instance.Auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
-                if (task.IsCanceled)
-                {
-                    Log.ColorError("CreateUserWithEmailAndPasswordAsync was canceled.", this);
-                    return;
-                }
-                if (task.IsFaulted)
-                {
-                    Log.ColorError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception, this);
-                    return;
-                }
-                // Firebase user has been created.
-                //Firebase.Auth.FirebaseUser user = FirebaseManager.Instance.User;
-                FirebaseManager.Instance.User = task.Result;
-
-                Debug.LogFormat("Firebase user created successfully: {0} ({1})",
-                        FirebaseManager.Instance.User.DisplayName,
-                        FirebaseManager.Instance.User.UserId);
+            try
+            {
+                var createdResult = await FirebaseManager.Instance.Auth.CreateUserWithEmailAndPasswordAsync(email, password);                
+                FirebaseManager.Instance.User = createdResult;
+                _firebaseUser = FirebaseManager.Instance.User;
+                Debug.LogFormat("Firebase user created successfully: {0} ({1},{2}) ",
+                            FirebaseManager.Instance.User.DisplayName,
+                            FirebaseManager.Instance.User.UserId,
+                            FirebaseManager.Instance.User.Email);
                 PopupManager.Instance.PrintMessage("Firebase user created successfully: " +
-                        FirebaseManager.Instance.User.UserId);
+                        FirebaseManager.Instance.User.Email);
 
                 UserProfile profile = new UserProfile();
-                profile.DisplayName = User.properties["name"]; // TODO: Change for Username
-                //profile.PhotoUrl = ; // TODO: Set Photo URL
+                // TODO: Change for Username
+                profile.DisplayName = User.properties["name"];
+                // TODO: Set Photo URL
+                //string url;
+                //profile.PhotoUrl = url; 
+                await FirebaseManager.Instance.User.UpdateUserProfileAsync(profile);
 
-                //FirebaseManager.Instance.User.UpdateUserProfileAsync(profile).ContinueWith(profileTask =>
-                //{
-                //    if (profileTask.IsCanceled)
-                //    {
-                //        Log.ColorError("UpdateUserProfileAsync was canceled.", this);
-                //        return;
-                //    }
-                //    if (profileTask.IsFaulted)
-                //    {
-                //        Log.ColorError("UpdateUserProfileAsync encountered an error: " + profileTask.Exception, this);
-                //        return;
-                //    }
-                //    Save(User);
+                string json = JsonUtility.ToJson(User);
+                await _database.GetReference(_firebaseUser.UserId).SetRawJsonValueAsync(json);
+                SaveJSONLocal(json);
 
-                    
-                //});
-            });
+                Debug.LogFormat("User data saved: {0} ({1})",
+                    _firebaseUser.DisplayName, _firebaseUser.UserId);
+                PopupManager.Instance.PrintMessage("User saved: " + FirebaseManager.Instance.User.DisplayName);
+            }
+            catch (AggregateException ex)
+            {
+                // The exception will be caught because you've awaited the call in an async method.
+                Log.ColorError("CreateUserWithEmailAndPasswordAsync encountered an error: " + ex.Message + " Data: " + ex.Data.Values, this);
+                PopupManager.Instance.PrintMessage("Creating error: " + ex.Message);
+            }           
         }
 
-        public void LogInUser()
+        public async void LogIn()
         {
             string email = temp["email"];
             string password = temp["password"];
-            Log.Color("Log In User: " + email, this);
-            FirebaseManager.Instance.Auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(signTask => {
-                if (signTask.IsCanceled)
-                {
-                    Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
-                    return;
-                }
-                if (signTask.IsFaulted)
-                {
-                    Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + signTask.Exception);
-                    return;
-                }
-                FirebaseManager.Instance.User = signTask.Result;
+            Log.Color("Logging In User: " + email, this);
 
+            try
+            {
+                var loginResult = await FirebaseManager.Instance.Auth.SignInWithEmailAndPasswordAsync(email, password);
+                FirebaseManager.Instance.User = loginResult;
                 _firebaseUser = FirebaseManager.Instance.User;
-                
-                _database.GetReference(_firebaseUser.UserId).GetValueAsync().ContinueWith(dataTask =>{
-                    if (dataTask.IsFaulted)
-                    {
-                        // Handle the error...
-                    }
-                    else if (dataTask.IsCompleted)
-                    {
-                        DataSnapshot dss = dataTask.Result;
-                        // Do something with snapshot...
+                Debug.LogFormat("Firebase user retrieved successfully: {0} ({1},{2}) ",
+                            FirebaseManager.Instance.User.DisplayName,
+                            FirebaseManager.Instance.User.UserId,
+                            FirebaseManager.Instance.User.Email);
+                PopupManager.Instance.PrintMessage("Firebase user logged successfully: " +
+                        FirebaseManager.Instance.User.Email);
 
-                        OnDataLoaded(dss.GetRawJsonValue());
-                        Save(User);
+                DataSnapshot dss = await _database.GetReference(_firebaseUser.UserId).GetValueAsync();
+                string json = dss.GetRawJsonValue();
+                User = GetFromJson(json);
+                SaveJSONLocal(json);
 
-                        Debug.LogFormat("User signed in successfully: {0} ({1})",
-                            _firebaseUser.DisplayName,_firebaseUser.UserId);
-                        PopupManager.Instance.PrintMessage("User created: " + FirebaseManager.Instance.User.UserId);
-                        //AppManager.Instance.ChangeSceneDelayBefore("MainMenuScene");
-                    }
-                });
-            });
+                Debug.LogFormat("User logged in successfully: {0} ({1})",
+                    _firebaseUser.DisplayName, _firebaseUser.UserId);
+                PopupManager.Instance.PrintMessage("User logged: " + FirebaseManager.Instance.User.UserId);                
+            }
+            catch (AggregateException ex)
+            {
+                // The exception will be caught because you've awaited the call in an async method.
+                Log.ColorError("SignInWithEmailAndPasswordAsync encountered an error: " + ex.Message + " Data: " + ex.Data.Values, this);
+                PopupManager.Instance.PrintMessage("Logging error: " + ex.Message);
+            }
         }
 
-        public void SignOutUser()
+        public void SignOut()
         {
             Log.Color("Signing out user: " + _firebaseUser.DisplayName, this);
             PopupManager.Instance.PrintMessage("Signing out: " + _firebaseUser.DisplayName);
@@ -151,15 +137,13 @@ namespace Fisiomed.User
 
         void Awake()
         {
-            //StartCoroutine(Downloader.Instance.LoadJSON(filePath, OnDataLoaded));
-
             string path = Application.persistentDataPath;
             filePath = Path.Combine(path, filename);
 
             if (File.Exists(filePath))
             {
                 Log.Color("File path:" + filePath, this);
-                OnDataLoaded(File.ReadAllText(filePath));
+                User = GetFromJson(File.ReadAllText(filePath));
             }
             else
             {
@@ -170,37 +154,25 @@ namespace Fisiomed.User
         {
             _database = FirebaseDatabase.DefaultInstance;
         }
-
-        private void OnDataLoaded(string json)
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                SignOut();
+            }
+        }
+        private User GetFromJson(string json)
         {
             Log.Color(json, this);
-            User = JsonUtility.FromJson<User>(json);
-            Log.Color(User.ToString(), this);
+            User user = JsonUtility.FromJson<User>(json);
+            Log.Color(user.ToString(), this);
+            return user;
         }
-
-        private void Save(User u)
-        {
-            string json = JsonUtility.ToJson(u);
-            SaveJSONLocal(json);
-            SaveJSONDatabase(json);
-        }
-
         private void SaveJSONLocal(string json)
         {
             Log.Color("Saving JSON Local: " + json, this);
             File.WriteAllText(filePath, json);
-        }        
-
-        private void SaveJSONDatabase(string json)
-        {
-            Log.Color("Saving JSON Database: " + json, this);
-            _database.GetReference(_firebaseUser.UserId).SetRawJsonValueAsync(json)
-            .ContinueWith(dbtask =>
-            {
-                AppManager.Instance.ChangeScene("MainMenuScene");
-            });
         }
-
         private void PrintUser(FirebaseUser user)
         {
             Log.Color("Current User: " + user.DisplayName, this);
@@ -213,7 +185,6 @@ namespace Fisiomed.User
         //    var dataSnapshot = await _database.GetReference(fUser.UserId).SetRawJsonValueAsync(userData);
         //    return dataSnapshot.Exists;
         //}
-
         //void OnEnable()
         //{
         //    SceneManager.sceneLoaded += OnSceneLoaded;
