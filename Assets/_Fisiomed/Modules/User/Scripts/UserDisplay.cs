7199bac78bba07e71c1999498c58c5f7afa52c44
Jaime Hernandez <jaimevazz@gmail.com>
2020-07-12 19:32:38 -0500

    Generic User DIsplay

--- Assets/_Fisiomed/Modules/User/Scripts/UserDisplay.cs
+++ Assets/_Fisiomed/Modules/User/Scripts/UserDisplay.cs
@@ -1,16 +1,26 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using TMPro;
 
 namespace Fisiomed.User
 {
     public class UserDisplay : MonoBehaviour
     {
-        [SerializeField] new TMP_Text name = default;
+        [SerializeField] string format = "{0}";
+        [SerializeField] string[] keys = { "name" };
+        [SerializeField] TMP_Text text = default;
+        List<string> values = new List<string>();
+        string content;
 
         void Start()
         {
-            name.text = UserManager.Instance.User.properties["name"];
+            for (int i = 0; i < keys.Length; i++)
+            {
+                values.Add(UserManager.Instance.User.properties[keys[i]]);
+            }
+            content = string.Format(format, values.ToArray());
+            text.text = content;     
         }
     }
 }
+ 
